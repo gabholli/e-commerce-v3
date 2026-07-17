b5f41998a78951a37510bc6cf22d55800da9cb2c
Gabe Hollifield <gholl467@gmail.com>
2026-07-17 16:30:04 -0700

    Added products API call to home page

--- src/frontend/pages/home.tsx
+++ src/frontend/pages/home.tsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react"
+import Loading from "../components/Loading"
+import axios from "axios"
+import type { allProductsInterface } from "../../types/types"
 
 export default function Home() {
+
+    const [products, setProducts] = useState<allProductsInterface[]>()
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        setLoading(true)
+        axios.get('https://fakestoreapi.com/products')
+            .then(response => {
+                setProducts(response.data)
+                setLoading(false)
+            })
+            .catch(error => {
+                console.error(error)
+                setLoading(false)
+            })
+
+    }, [])
+
+    const allProducts = products?.map((product) => {
+        return (
+            <div>
+                <h1>{product.title}</h1>
+            </div>
+        )
+    })
+
+    if (loading) {
+        return <Loading />
+    }
+
     return (
-        <div>Home</div>
+        <main>
+            <div>
+                {allProducts}
+            </div>
+        </main>
     )
 }
