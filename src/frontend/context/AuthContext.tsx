cb4e55c25b5a71457cc419459573e268f85b2c88
Gabe Hollifield <gholl467@gmail.com>
2026-07-18 14:45:50 -0700

    Added code for handling React context

---
+++ src/frontend/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+// import { createContext, useEffect, useState, useContext } from "react"
+// import type { ReactNode } from "react"
+// import type { AuthContextType } from "../../types/types"
+// import { checkAuth } from "../../utils/checkAuth"
+
+// const AuthContext = createContext<AuthContextType>({
+//     loggedIn: false,
+//     setLoggedIn: () => { }
+// })
+
+// export default function AuthContextProvider({ children }: { children: ReactNode }) {
+
+//     const [loggedIn, setLoggedIn] = useState<boolean>(false)
+
+//     useEffect(() => {
+//         async function isSignedIn() {
+//             const checkAuthValue = await checkAuth()
+//             setLoggedIn(checkAuthValue)
+//         }
+//         isSignedIn()
+//     }, [])
+
+//     return (
+//         <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
+//             {children}
+//         </AuthContext.Provider>
+//     )
+// }
+
+// export function UserAuth() {
+//     return useContext(AuthContext)
+// }
