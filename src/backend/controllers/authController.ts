cd087a03de3ca464c2b7d0c3d665251e7b634bfe
Gabe Hollifield <gholl467@gmail.com>
2026-07-24 13:22:12 -0700

    Added more function to auth controller file
    Finished register function and added login and logout functions

--- src/backend/controllers/authController.ts
+++ src/backend/controllers/authController.ts
@@ -2,6 +2,7 @@
 // import { Request, Response } from "express"
 // import getDBConnection from "../db/db"
 // import bcrypt from "bcryptjs"
+// import { error } from "console"
 
 // export async function registerUser(req: Request, res: Response) {
 
@@ -41,6 +42,62 @@
 
 //         req.session.userId = result.lastID
 
-//         res.status(500).json({ error: "Registration failed. Please try again" })
+//         res.status(201).json({ message: "User registered" })
+
+//     } catch (err) {
+//         if (err instanceof Error) {
+//             console.error("Registration error: ", err.message)
+//             res.status(500).json({ error: "Registration failed. Please try again" })
+//         }
+
 //     }
+// }
+
+// export async function loginUser(req: Request, res: Response) {
+
+//     let { email, password } = req.body
+
+//     if (!email || !password) {
+//         return res.status(400).json({ error: "All fields are required" })
+
+//     }
+
+//     email = email.trim()
+
+//     try {
+
+//         const db = await getDBConnection()
+
+//         const user = await.db.get("SELECT * FROM users WHERE email = ?", [email])
+
+//         if (!user) {
+//             return res.status(401).json({ error: "Invalid credentials" })
+//         }
+
+//         const isvalid = await bcrypt.compare(password, user.password)
+
+//         if (!isvalid) {
+
+//             return res.status(401).json({ error: "Invalid credentials" })
+
+//         }
+
+//         req.sessions.userId = user.id
+
+//         res.json({ message: "Logged in" })
+
+//     } catch (err) {
+//         if (err instanceof Error) {
+//             console.error("Login error: ", err.message)
+//             res.status(500).json({ error: "Login failed. Please try again" })
+//         }
+//     }
+// }
+
+// export async function logoutUser(req: Request, res: Response) {
+
+//     req.session.destroy(() => {
+
+//         res.json({ message: "Logged out" })
+//     })
 // }
