4c904697f9079a39314edd4f7482b9820e0be11c
Gabe Hollifield <gholl467@gmail.com>
2026-07-27 20:26:01 -0700

    Updated function names in cart queries file

--- src/backend/models/cartModels.ts
+++ src/backend/models/cartModels.ts
@@ -1,34 +1,34 @@
 import getDBConnection from "../db/db.ts";
 
-export async function checkExistingInCart(userId: number, productId: number) {
+export async function checkExistingInCartQuery(userId: number, productId: number) {
     const db = await getDBConnection()
 
     return db.get('SELECT * FROM cart_items WHERE user_id = ? AND product_id = ?',
         [userId, productId])
 }
 
-export async function updateCart(id: number, userId: number) {
+export async function updateCartQuery(id: number, userId: number) {
     const db = await getDBConnection()
 
     return db.run('UPDATE cart_items SET quantity = quantity + 1 WHERE id = ? AND user_id = ?',
         [id, userId])
 }
 
-export async function insertIntoCart(userId: number, productId: number) {
+export async function insertIntoCartQuery(userId: number, productId: number) {
     const db = await getDBConnection()
 
     return db.run('INSERT INTO cart_items (user_id, product_id, quantity) VALUES (?, ?, 1)',
         [userId, productId])
 }
 
-export async function getCountFromCart(sessionId: number) {
+export async function getCountFromCartQuery(sessionId: number) {
     const db = await getDBConnection()
 
     return db.get(`SELECT SUM(quantity) AS totalItems FROM cart_items WHERE user_id = ?`,
         [sessionId])
 }
 
-export async function getAllFromCart(sessionId: number) {
+export async function getAllFromCartQuery(sessionId: number) {
     const db = await getDBConnection()
 
     return db.all(
@@ -39,7 +39,7 @@ export async function getAllFromCart(sessionId: number) {
     )
 }
 
-export async function selectCartItemQuantity(itemId: number, sessionId: number) {
+export async function selectCartItemQuantityQuery(itemId: number, sessionId: number) {
     const db = await getDBConnection()
 
     return db.get('SELECT quantity FROM cart_items WHERE id = ? AND user_id = ?',
