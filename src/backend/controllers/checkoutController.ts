ab9d23685ecd7198eb457ef90304bef77c7ce4b5
Gabe Hollifield <gholl467@gmail.com>
2026-07-29 12:30:50 -0700

    Changed files back

--- src/backend/controllers/checkoutController.ts
+++
@@ -1,5 +0,0 @@
-import { type Request, type Response } from "express"
-
-export async function createCheckoutSession(req: Request, res: Response) {
-
-}
--- src/backend/routes/checkoutRoutes.ts
+++
@@ -1,7 +0,0 @@
-// routes/checkout.ts
-import express from "express"
-import { createCheckoutSession } from "../controllers/checkoutController"
-
-export const checkoutRouter = express.Router()
-
-checkoutRouter.post("/create-checkout-session", createCheckoutSession)
