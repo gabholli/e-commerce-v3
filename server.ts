32c9c204a5aaa2f949c6b71c6e49e758ed126b22
Gabe Hollifield <gholl467@gmail.com>
2026-07-30 15:17:45 -0700

    Started updated backend files to support MongoDB

--- server.ts
+++ server.ts
@@ -3,12 +3,10 @@ import express from "express"
 import cors from "cors"
 import createTable from "./src/backend/createTable.ts"
 import session from "express-session"
-import memorystore from "memorystore"
 import { authRouter } from "./src/backend/routes/authRoutes.ts"
 import { meRouter } from "./src/backend/routes/meRoutes.ts"
 import { cartRouter } from "./src/backend/routes/cartRoutes.ts"
-
-const MemoryStore = memorystore(session)
+import MongoStore from "connect-mongo"
 
 const app = express()
 
@@ -26,11 +24,19 @@ if (!process.env.SESSION_SECRET) {
     throw new Error("SESSION_SECRET environment variable is not set!")
 }
 
+const mongoUri = process.env.ATLAS_URI
+
+if (!mongoUri) {
+    throw new Error("ATLAS_URI environment variable is not set")
+}
+
 app.use(express.json())
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use(session({
-    store: new MemoryStore({
-        checkPeriod: 86400000
+    store: MongoStore.create({
+        mongoUrl: mongoUri
     }),
     secret: process.env.SESSION_SECRET,
     resave: false,
@@ -50,8 +56,4 @@ app.use((_req, res) => {
     res.send("Server here for e-commerce site...")
 })
 
-createTable().then(() => {
-    app.listen(PORT, () => console.log("Server connected..."))
-}).catch((err) => {
-    console.error("Failed to start server:", err)
-})
+app.listen(PORT, () => console.log("Server connected..."))
