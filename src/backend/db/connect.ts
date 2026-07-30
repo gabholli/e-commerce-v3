49e66c535afb3dd3479373c99b8d9f25f95fed41
Gabe Hollifield <gholl467@gmail.com>
2026-07-30 15:22:23 -0700

    Updated database file to support mongoDB and its title

---
+++ src/backend/db/connect.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import { MongoClient } from "mongodb"
+const connectionString = process.env.ATLAS_URI || ""
+const client = new MongoClient(connectionString)
+let conn: any
+try {
+    conn = await client.connect()
+} catch (e) {
+    console.error(e)
+}
+let db = conn.db("e-commerce-v3")
+export default db
