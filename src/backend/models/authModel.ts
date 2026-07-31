df89a12cf9b6fd4ea85f55a2ccb8cffea669ee6e
Gabe Hollifield <gholl467@gmail.com>
2026-07-31 14:23:09 -0700

    Changed auth model functions to use MongoDB

--- src/backend/models/authModel.ts
+++ src/backend/models/authModel.ts
@@ -1,18 +1,29 @@
 import db from "../db/connect.ts";
 
 export async function findUserByEmail(email: string) {
-    let collection = db.collection("users")
+    const collection = db.collection("users")
     return collection.findOne({ email })
 }
 
 export async function findUserIdByEmail(email: string) {
 
-    return db.get("SELECT id FROM users WHERE email = ?",
-        [email])
+    const collection = db.collection("users")
+    return collection
+        .findOne({ email })
 }
 
 export async function createUser(email: string, hashedPassword: string) {
-
-    return db.run('INSERT INTO users (email, password) VALUES (?, ?)',
-        [email, hashedPassword])
+    const collection = db.collection("users")
+    return collection.updateOne(
+        {
+            email
+        },
+        {
+            $set: {
+                password: hashedPassword
+            }
+        },
+        { upsert: true }
+    )
 }
+
