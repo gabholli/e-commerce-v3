1461a2cfff121efec0f0d0bdd4370d8133bc65da
Gabe Hollifield <gholl467@gmail.com>
2026-08-03 14:42:30 -0700

    Add publishable Stripe key

--- src/frontend/components/StripeContainer.tsx
+++ src/frontend/components/StripeContainer.tsx
@@ -1,5 +1,7 @@
 import PaymentForm from "./PaymentForm";
 
+const PUBLIC_KEY = "pk_test_51U04nq2fNJpTHnByzuJrp0UtfZzmKQhEWlImWBdnsrNHhffBvbklh6e9gvFW1dicO8nadM2T0VYRS2V6sWZqjcMs00yc2DdbCO"
+
 export default function StripeContainer({ onBack }: { onBack: () => void }) {
     return (
         <PaymentForm
