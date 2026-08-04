4c8787ff10c0f1b4e02f5df74b6e1a1f8113e340
Gabe Hollifield <gholl467@gmail.com>
2026-08-03 23:32:06 -0700

    Reveted changes from last commit

--- src/frontend/api.ts
+++ src/frontend/api.ts
@@ -1,16 +1,10 @@
 import axios from "axios"
 
-// const api = axios.create({
-//     baseURL: import.meta.env.DEV
-//         ? "http://localhost:3001"
-//         : "https://e-commerce-v3-qb17.onrender.com",
-//     withCredentials: true
-// })
-
 const api = axios.create({
-    baseURL: "https://e-commerce-v3-qb17.onrender.com",
+    baseURL: import.meta.env.DEV
+        ? "http://localhost:3001"
+        : "https://e-commerce-v3-qb17.onrender.com",
     withCredentials: true
 })
-
 
 export default api
