e30c4fc1e088f07cf932296c5e7ff35c916f2738
Gabe Hollifield <gholl467@gmail.com>
2026-08-03 22:38:16 -0700

    Removed trailing slash in CORS object to fix Netlify error

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ const app = express()
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://gabholli-e-commerce-site.netlify.app/"
+        "https://gabholli-e-commerce-site.netlify.app"
     ],
     credentials: true
 }))
