07627a0b560dbb86bba89156b2daa0ab337b457b
Gabe Hollifield <gholl467@gmail.com>
2026-08-03 23:30:34 -0700

    Tried not using Vite env variable in api file

--- src/frontend/api.ts
+++ src/frontend/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios"
 
+// const api = axios.create({
+//     baseURL: import.meta.env.DEV
+//         ? "http://localhost:3001"
+//         : "https://e-commerce-v3-qb17.onrender.com",
+//     withCredentials: true
+// })
+
 const api = axios.create({
-    baseURL: import.meta.env.DEV
-        ? "http://localhost:3001"
-        : "https://e-commerce-v3-qb17.onrender.com",
+    baseURL: "https://e-commerce-v3-qb17.onrender.com",
     withCredentials: true
 })
+
 
 export default api
