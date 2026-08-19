638c76425907fb3cb1518cadd7e846f2171e906e
Gabe Hollifield <gholl467@gmail.com>
2026-08-19 09:48:59 -0700

    Added current price paid to success screen

--- src/frontend/pages/success.tsx
+++ src/frontend/pages/success.tsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router"
+import { Link, useLocation } from "react-router"
 
 export default function Success() {
+
+    const location = useLocation()
+    const currentPrice = location.state.amount
+
     return (
         <div className="flex flex-col flex-1 justify-center items-center
             gap-y-8 px-2">
             <h1 className="text-center text-3xl">
-                Your payment was successful!
+                `Your payment for $${currentPrice} was successful!`
             </h1>
             <Link to="/" className="bg-green-500 px-4 py-2 rounded-3xl text-white text-xl hover:underline">
                 Return to home
