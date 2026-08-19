c652e31c23ae34e1d85f382f95ff1d51df4218f1
Gabe Hollifield <gholl467@gmail.com>
2026-08-19 10:40:12 -0700

    Defined environment variable explicity in rate limiter file

--- src/backend/middleware/authRateLimited.ts
+++ src/backend/middleware/authRateLimited.ts
@@ -3,6 +3,12 @@ import rateLimit from "express-rate-limit"
 // @ts-expect-error Missing declaration file for the JavaScript package.
 import MongoStore from "rate-limit-mongo"
 
+const mongoUri = process.env.ATLAS_URI
+
+if (!mongoUri) {
+    throw new Error("ATLAS_URI environment variable is not set!")
+}
+
 export const authRateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     limit: 10,
@@ -10,7 +16,7 @@ export const authRateLimiter = rateLimit({
     legacyHeaders: false,
     message: { error: "Too many attempts, please try again in 15 minutes" },
     store: new MongoStore({
-        url: process.env.ATLAS_URI!,
+        url: mongoUri,
         collectionName: "rateLimits",
         expireTimeMs: 15 * 60 * 1000
     })
