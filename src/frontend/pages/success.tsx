2ffae1d6b9aefaebb649942c23e3a99d70428b56
Gabe Hollifield <gholl467@gmail.com>
2026-08-19 09:50:57 -0700

    Removed unnecessary syntax from success text

--- src/frontend/pages/success.tsx
+++ src/frontend/pages/success.tsx
@@ -9,7 +9,7 @@ export default function Success() {
         <div className="flex flex-col flex-1 justify-center items-center
             gap-y-8 px-2">
             <h1 className="text-center text-3xl">
-                `Your payment for $${currentPrice} was successful!`
+                Your payment for ${currentPrice} was successful!
             </h1>
             <Link to="/" className="bg-green-500 px-4 py-2 rounded-3xl text-white text-xl hover:underline">
                 Return to home
